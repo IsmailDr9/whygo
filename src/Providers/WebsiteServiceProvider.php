33c49f01179462940f3252b68a11f8cc1bf42199
ismail_dr_makane92 <ismaildarwish92@gmail.com>
2021-10-25 13:28:22 +0300

    first commit

---
+++ src/Providers/WebsiteServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Whygo\Website\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebsiteServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        dd('yes');
+
+    }
+
+    public function register()
+    {
+
+    }
+}
